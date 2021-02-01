4ad6ec3bf23409278dc40dabc9c8afea826ab089
Nam Tran <nam.tran.ext@personio.de>
2021-02-01 23:20:11 +0700

    grmbl

--- february-leetcoding-challenge-2021/week-1/hamming-weight.cpp
+++ february-leetcoding-challenge-2021/week-1/hamming-weight.cpp
@@ -6,10 +6,10 @@ public:
     int hammingWeight(uint32_t n) {
         int c = 0;
         while (n) {
-            if ((n&1) == 1) {
+            if ((n&1) == 1) { // indicate that the most right bit is 1 or not
                 c++;
             }
-            n = n>>1;
+            n = n>>1; // remove the most right bit
         }
 
         return c;
